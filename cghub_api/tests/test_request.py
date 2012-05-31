cc8270f3764cad26c8d077114538605386fe3805
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-05-31 19:13:26 +0300

    ticket:17 new tests and data for test

--- cghub_api/tests/test_request.py
+++ cghub_api/tests/test_request.py
@@ -8,7 +8,7 @@ from cghub_api.api import request
 
 
 class RequestTest(unittest.TestCase):
-    """Test requests."""
+    """Test request."""
 
     def test_query_required(self):
         """Test that QuerryRequired exception is raised 
@@ -21,6 +21,19 @@ class RequestTest(unittest.TestCase):
         TODO: re-write this test to raise exception if query is not allowed."""
         self.assertEqual(request(query='test'), None)
 
+    def test_no_file_exists(self):
+        """
+        Test that exception is raised when file does not exist.
+        """
+        self.assertRaises(IOError, request, None, "no_such_file.xml")
+
+    def test_no_exeption_for_existing_file(self):
+        """
+        Test that no exception is raised when file exists.
+        """
+        import os
+        print os.getcwd()
+        self.assertEqual(request(file_name='tests/test_data/aliquot_id.xml'), None)
 
 if __name__ == '__main__':
     unittest.main()
