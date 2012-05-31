43de0cb47e5f41051102ebe73bc121e94d378da5
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-05-31 22:44:12 +0300

    ticket:17 added basic classes for Request and Response

---
+++ cghub_api/cghub_api/models.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+"""
+cghub_api.models
+~~~~~~~~~~~~~~~~
+
+This module contains the primary objects that power Requests.
+"""
+
+from .exceptions import (
+        RequestException, QueryRequired)
+
+
+class Request(object):
+    """The :class:`Request <Request>` object. It carries out all functionality of
+    Requests. Recommended interface is with the Requests functions.
+    """
+
+    def __init__(self,
+            query=None,
+            filename=None):
+        self.query = query
+
+    def __repr__(self):
+        return '<Request [%s]>' % (self.query)
+
+    def send(self, anyway=False, prefetch=False):
+        """
+        """
+        pass
+
+
+class Response(object):
+    """The core :class:`Response <Response>` object. All
+    :class:`Request <Request>` objects contain a
+    :class:`response <Response>` attribute, which is an instance
+    of this class.
+    """
+
+    def __init__(self):
+
+        #: Resulting :class:`HTTPError` of request, if one occurred.
+        self.error = None
+
+        #: Encoding to decode with when accessing r.text.
+        self.encoding = None
+
+        #: The :class:`Request <Request>` that created the Response.
+        self.request = None
+
+        #: Dictionary of configurations for this request.
+        self.config = {}
+
+    def __repr__(self):
+        return '<Response to request [%s]>' % (self.request)
