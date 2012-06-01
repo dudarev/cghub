7f6c150f8fdcda5c2d232608c422ebe555d6902f
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-06-01 11:22:02 +0300

    added cghub_api/__init__.py

---
+++ cghub_api/cghub_api/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+cghub_api
+~~~~~~~~~
+
+:copyright: (c) 2012 CGHub UCSC
+:license: New BSD, see ../LICENSE for more details.
+
+"""
+
+__title__ = 'cghub_api'
+__version__ = '0.0.1'
+__author__ = 'Artem Dudarev'
+__license__ = 'New BSD'
+__copyright__ = 'Copyright 2012 CGHub UCSC'
+
+
+from .models import Request, Response
+from .api import request
+from .exceptions import (
+    RequestException, QueryRequired,
+)
