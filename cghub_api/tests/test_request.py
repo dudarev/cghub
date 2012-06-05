446babe82ecadf603bd4f0f1fc98c29a9c466155
Artem Dudarev for 42cc <dudarev+42cc@gmail.com>
2012-06-05 23:02:28 +0300

    ticket:18 added new test for run_xml

--- cghub_api/tests/test_request.py
+++ cghub_api/tests/test_request.py
@@ -57,6 +57,15 @@ class RequestTest(unittest.TestCase):
         self.assertEqual(first_analysis_title, 
                 'NHGRI_TCGA Sequence Alignment/Map for SAMPLE:TCGA:TCGA-55-1594-11A-01D-1040-01:SRS127193')
 
+    def test_item_has_run_instance(self):
+        """
+        Test via analysis title of that instance.
+        """
+        results = request(file_name='tests/test_data/aliquot_id.xml')
+        first_run_experiment_ref = results.Result[0].run_xml.RUN_SET[0].RUN[0].EXPERIMENT_REF
+        self.assertEqual(first_run_experiment_ref.attrib['refname'], 
+                '7290.WR24924.Catch-62054.B045FABXX110327.P')
+
 
 if __name__ == '__main__':
     unittest.main()
