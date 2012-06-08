dc041173005539f1955c736dbb7c2c0f89e37912
Artem Dudarev <dudarev+42cc@gmail.com>
2012-06-08 15:35:36 +0300

    ticket:12 added search view

--- cghub/apps/core/views.py
+++ cghub/apps/core/views.py
@@ -1,4 +1,18 @@
 from django.views.generic.base import TemplateView
+from cghub_api.api import request as api_request
+
 
 class HomeView(TemplateView):
     template_name = 'core/home.html'
+
+
+class SearchView(TemplateView):
+    template_name = 'core/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        if self.request.GET.get('q'):
+            results = api_request(query="xml_text=6d7*")
+            len(results)
+            context['data'] = len(results.Result)
+        return context
--- cghub/urls.py
+++ cghub/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from cghub.apps.core.views import HomeView
+from cghub.apps.core.views import HomeView, SearchView
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home_page'),
+    url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^cart/', include('cghub.apps.cart.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
