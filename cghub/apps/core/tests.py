c2002124c52ad92e94da6347a13874d93e089f8d
Artem Dudarev <dudarev+42cc@gmail.com>
2012-06-11 23:34:45 +0300

    ticket:12 moved tests to tests.py file

--- cghub/apps/core/tests.py
+++ cghub/apps/core/tests.py
@@ -5,12 +5,17 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test.testcases import TestCase
 
+class CoreTests(TestCase):
+    def test_index(self):
+        """
+        Tests that index page exists.
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_non_existen_search(self):
+        response = self.client.get('/search/?q=non_existent_search_query')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('No results found' in response.content)
