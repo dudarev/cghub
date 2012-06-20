f8a925cb241a700abdb7ffa75d851f15cf2d8ce0
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2012-06-20 11:04:28 +0300

    ticket:9 Add sceleton view for manifest xml donloading

--- cghub/apps/cart/views.py
+++ cghub/apps/cart/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, View
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.utils import simplejson as json
@@ -9,14 +9,16 @@ from cghub.apps.cart.utils import get_or_create_cart, get_cart_stats
 class CartView(TemplateView):
     """ Lists files in cart """
     template_name = 'cart/cart.html'
-   
+
     def get_context_data(self, **kwargs):
         return {'results': get_or_create_cart(self.request),
-            'stats': get_cart_stats(self.request)
+                'stats': get_cart_stats(self.request)
         }
 
-class CartAddRemoveFilesView(TemplateView):
+
+class CartAddRemoveFilesView(View):
     """ Handles files added to cart """
+
     def post(self, request, action):
         if 'add' == action:
             # get all additional attributes of files
@@ -24,10 +26,23 @@ class CartAddRemoveFilesView(TemplateView):
             for f in request.POST.getlist('selected_files'):
                 add_file_to_cart(request, attributes[f])
             return HttpResponse(json.dumps({"redirect": reverse('cart_page')}), mimetype="application/json")
-                
         if 'remove' == action:
             for f in request.POST.getlist('selected_files'):
                 # remove file from cart by sample id
                 remove_file_from_cart(request, f)
             return HttpResponseRedirect(reverse('cart_page'))
-        
+
+
+class CartDownloadFilesView(View):
+    def post(self, request, action):
+        if 'manifest' == action:
+            self.manifest()
+        elif 'xml' == action:
+            self.xml()
+        return HttpResponseRedirect(reverse('cart_page'))
+
+    def manifest(self):
+        pass
+
+    def xml(self):
+        pass
