371fcf8b8592a87ae4ebc50dc6e1995ac6af4c3c
Artem Dudarev <dudarev+42cc@gmail.com>
2012-06-26 00:33:06 +0300

    ticket:42 whitespace

--- cghub_api/cghub_api/api.py
+++ cghub_api/cghub_api/api.py
@@ -24,18 +24,18 @@ def sort_results(results, sort_by):
     from operator import itemgetter
 
     reverse_order = False
-
+    
     # figure out order
     if sort_by.startswith("-"):
         reverse_order = True
         sort_by = sort_by[1:]
-
+    
     try:
         sorted_list = sorted(results, key=itemgetter(sort_by))
     except AttributeError:
         # no such child, return original unsorted result
         return results
-
+    
     # reverse order if needed
     if reverse_order:
         sorted_list.reverse()
