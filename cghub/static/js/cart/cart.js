c21f5067d34cfd16ac871804f2558b67537b3052
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2012-06-26 20:49:18 +0300

    ticket:45 Remove unused js code from the car js module

--- cghub/static/js/cart/cart.js
+++ cghub/static/js/cart/cart.js
@@ -19,32 +19,11 @@ jQuery(function ($) {
             cghub.cart.$downloadManifestBtn = $('.btn.cart-form-download-manifest');
             cghub.cart.$downloadXmlBtn = $('.btn.cart-form-download-xml');
             cghub.cart.$removeBtn = $('.btn.cart-form-remove');
-            cghub.cart.$addFilesForm = $('form#id_add_files_form');
         },
         bindEvents:function () {
             cghub.cart.$downloadManifestBtn.on('click', cghub.cart.downloadManifest);
             cghub.cart.$downloadXmlBtn.on('click', cghub.cart.downloadXml);
             cghub.cart.$removeBtn.on('click', cghub.cart.removeFromCart);
-            cghub.cart.$addFilesForm.on('submit', cghub.cart.addFilesFormSubmit);
-        },
-        addFilesFormSubmit:function () {
-            // collect all data attributes
-            var data = {};
-            var selected_files = $('input[type="checkbox"][name="selected_files"]:checked');
-            selected_files.each(function (i, f) {
-                var file_data = $(f).data();
-                data[file_data.legacy_sample_id] = file_data;
-            });
-            $.ajax({
-                data:$(this).serialize() + "&attributes=" + JSON.stringify(data),
-                type:$(this).attr('method'),
-                dataType:'json',
-                url:$(this).attr('action'),
-                success:function (data) {
-                    window.location.href = data.redirect;
-                }
-            });
-            return false;
         },
         removeFromCart:function () {
             var btn = $(this);
