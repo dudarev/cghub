c90c822903ae8d0616d268f0481eba3b2e011af4
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2012-06-28 17:37:39 +0300

    ticket:48 Fix remove_file_from_cart

--- cghub/apps/cart/utils.py
+++ cghub/apps/cart/utils.py
@@ -15,13 +15,11 @@ def add_file_to_cart(request, file_dict):
     request.session.modified = True
 
 
-def remove_file_from_cart(request, legacy_sample_id):
+def remove_file_from_cart(request, analysis_id):
     """ removes file with legacy_sample_id from cart """
     cart = get_or_create_cart(request)
-    for i, file_dict in enumerate(cart):
-        if file_dict['legacy_sample_id'] == legacy_sample_id:
-            del(cart[i])
-            break
+    if analysis_id in cart:
+        del cart[analysis_id]
     request.session.modified = True
 
 
