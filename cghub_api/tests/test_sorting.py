c340d19a50de17a094d003dee2ffb6f7ae7ab17c
Artem Dudarev <dudarev+42cc@gmail.com>
2012-06-29 13:20:05 +0300

    ticket:49 test method to calculate files size

--- cghub_api/tests/test_sorting.py
+++ cghub_api/tests/test_sorting.py
@@ -14,7 +14,8 @@ class SortingTest(unittest.TestCase):
         Tests that total files size is calculated correctly.
         """
         results = request(file_name='tests/test_data/search_several_files.xml')
-
+        results.calculate_files_size()
+        self.assertEqual( results.Result[0].files_size, 8407199477+10497249326 )
 
 if __name__ == '__main__':
     unittest.main()
