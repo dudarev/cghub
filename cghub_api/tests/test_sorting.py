3ddb6121080a0cbb6c47120b8653cc3f77a5bfac
Artem Dudarev <dudarev+42cc@gmail.com>
2012-06-29 16:04:42 +0300

    ticket:49 test to sort by files size

--- cghub_api/tests/test_sorting.py
+++ cghub_api/tests/test_sorting.py
@@ -18,5 +18,14 @@ class SortingTest(unittest.TestCase):
         self.assertEqual(results.Result[0].files_size, 8407199477+10497249326)
         self.assertEqual(results.Result[1].files_size, 10497249326)
 
+    def test_sort_by_files_size(self):
+        results = request(file_name='tests/test_data/search_several_files.xml')
+        results.sort(sort_by='-files_size')
+        for i in range(len(results.Result)-1):
+            self.assertGreaterEqual(
+                results.Result[i].files_size,
+                results.Result[i+1].files_size,
+                msg='Files size is not sorted for elements: {0}, {1}'.format(i,i+1))
+
 if __name__ == '__main__':
     unittest.main()
