c6164c7f0b8d3a96ce33f27c2bf37da0d63e5823
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2012-07-10 21:47:05 +0300

    ticket:55 Add pagination test cases

--- cghub_api/tests/test_pagination.py
+++ cghub_api/tests/test_pagination.py
@@ -34,5 +34,27 @@ class PaginationTestCase(unittest.TestCase):
         self.default_results_count = len(self.default_results.findall('Result'))
 
 
+    def test_pagination_offset_and_limit_are_none(self):
+        results = request(query='xml_text=6d5*')
+        self.assertEqual(len(results.findall('Result')), self.default_results_count)
+
+    def test_pagination_offset_gt_size(self):
+        results = request(query='xml_text=6d5*', offset=self.default_results_count + 1)
+        self.assertEqual(len(results.findall('Result')), 0)
+
+    def test_pagination_offset_lt_zero(self):
+        results = request(query='xml_text=6d5*', offset=-3, limit=2)
+        self.assertEqual(len(results.findall('Result')), 2)
+
+    def test_pagination_offset_gt_zero_limit_gt_size(self):
+        offset = 3
+        limit = self.default_results_count + offset
+        results = request(query='xml_text=6d5*', offset=offset, limit=limit)
+        self.assertEqual(len(results.findall('Result')), self.default_results_count - offset)
+
+    def test_pagination_offset_gt_zero_and_lt_size_limit_gt_zero(self):
+        results = request(query='xml_text=6d5*', offset=3, limit=3)
+        self.assertEqual(len(results.findall('Result')), 3)
+
 if __name__ == '__main__':
     unittest.main()
