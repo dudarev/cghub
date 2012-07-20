350a875b47e12a01734c679b61466e9de53b74be
Artem Dudarev <dudarev@gmail.com>
2012-07-21 00:51:27 +0300

    ticket:21 modifications to figures

--- cghub_api/utils/profile/plot.py
+++ cghub_api/utils/profile/plot.py
@@ -7,13 +7,18 @@ def plot_profiler_stats(ifilename, ofilename):
     with open(ifilename, 'r') as ifile:
         statistics = csv.reader(ifile)
         statistics.next()
-        figure = plt.figure()
-        ax = figure.add_subplot(111)
+        count_ar = []
+        time_ar = []
         for row in statistics:
             query, count, time = row
-            ax.plot(int(count), float(time), 'o')
-            ax.plot([1, 1], label=query)
-        plt.savefig(ofilename, dpi=120)
+            count_ar.append(int(count))
+            time_ar.append(float(time))
+    figure = plt.figure()
+    ax = figure.add_subplot(111)
+    ax.plot(count_ar, time_ar, 'o')
+    plt.ylabel('Time [s]', fontsize=20)
+    plt.xlabel('Results count', fontsize=20)
+    plt.savefig(ofilename, dpi=120)
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
