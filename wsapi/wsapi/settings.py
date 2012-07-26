3bebd5780abd0023828bcaec115ddec3144e12e4
Evgeny Ivanov <jey.ivanov@gmail.com>
2012-07-26 14:55:32 +0300

    ticket:63 added USE_CACHE and CACHE_BACKEND params to the settings

--- wsapi/wsapi/settings.py
+++ wsapi/wsapi/settings.py
@@ -8,8 +8,12 @@ This module provides the some default settings.
 Settings:
 
     :CACHE_DIR: directory to store cache, ``/tmp/wsapi/`` by default
+    :USE_CACHE: enables caching if equals True, False is the default
+    :CACHE_BACKEND: determines cache type, for now available types are ('simple',)
 
 """
 
+USE_CACHE = False
 
+CACHE_BACKEND = 'simple'
 CACHE_DIR = '/tmp/wsapi/'
