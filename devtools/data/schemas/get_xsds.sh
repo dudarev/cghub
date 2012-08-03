9ed4521fbb83111a75d9077d92ee61faa6386efe
Artem Dudarev <dudarev@gmail.com>
2012-08-03 23:55:41 +0300

    ticket:66 moved schemas files to separate directories

--- devtools/data/schemas/get_xsds.sh
+++
@@ -1,4 +0,0 @@
-wget ftp://ftp.sra.ebi.ac.uk/meta/xsd/sra_1_3/SRA.common.xsd
-wget ftp://ftp.sra.ebi.ac.uk/meta/xsd/sra_1_3/SRA.analysis.xsd
-wget ftp://ftp.sra.ebi.ac.uk/meta/xsd/sra_1_3/SRA.experiment.xsd
-wget ftp://ftp.sra.ebi.ac.uk/meta/xsd/sra_1_3/SRA.run.xsd
