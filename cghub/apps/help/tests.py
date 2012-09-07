6306aece471160f87128f1fa381dba2bd8ec251e
Artem Dudarev <dudarev@gmail.com>
2012-09-07 16:09:12 +0300

    ticket:69 added test for link to help page

--- cghub/apps/help/tests.py
+++ cghub/apps/help/tests.py
@@ -1,16 +1,16 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Test help pages
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class HelpTest(TestCase):
+    def test_links_to_home(self):
+        response = self.client.get(reverse('help_cart_page'))
+        # two occurances in navigation bar /help/?from=/help/
+        # one in text
+        self.assertContains(response, '/help/', 3)
+        response = self.client.get(reverse('help_search_page'))
+        self.assertContains(response, '/help/', 3)
