77a4eda30b310d1ee71ab963f6a82882598fc693
Artem Dudarev <dudarev@gmail.com>
2012-09-24 18:54:53 +0300

    ticket:86 decreased times for cache for testing

--- cghub/settings/cart_cache.py
+++ cghub/settings/cart_cache.py
@@ -7,5 +7,5 @@ MEDIA_ROOT = root('media')
 CART_CACHE_FOLDER = os.path.join(MEDIA_ROOT, 'api_results_cache')
 MEDIA_URL = ''
 
-TIME_DELETE_CART_CACHE_FILES_OLDER = timedelta(hours=24)
-TIME_CHECK_CART_CACHE_INTERVAL = timedelta(hours=12)
+TIME_DELETE_CART_CACHE_FILES_OLDER = timedelta(hours=2)
+TIME_CHECK_CART_CACHE_INTERVAL = timedelta(hours=1)
