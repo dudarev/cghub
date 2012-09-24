b6539eb0a1a5fd9a161bf66cadb0209c2c92c442
Artem Dudarev <dudarev@gmail.com>
2012-09-24 19:51:53 +0300

    ticket:86 pep8 styling

--- cghub/apps/core/tests.py
+++ cghub/apps/core/tests.py
@@ -52,7 +52,8 @@ class TestTemplateTags(TestCase):
             'request': test_request
         }))
 
-        self.assertEqual(out,
+        self.assertEqual(
+            out,
             '<a class="sort-link" href="/search/?sort_by=last_modified">Date Uploaded</a>')
 
         # make sure that other request.GET variables are preserved
@@ -64,7 +65,8 @@ class TestTemplateTags(TestCase):
             'request': test_request
         }))
 
-        self.assertEqual(out,
+        self.assertEqual(
+            out,
             '<a class="sort-link" href="/search/?q=sample_query&amp;sort_by=last_modified">Date Uploaded</a>')
 
         # make sure that direction label is rendered if it is active sort filter
@@ -77,7 +79,8 @@ class TestTemplateTags(TestCase):
             'request': test_request
         }))
 
-        self.assertEqual(out,
+        self.assertEqual(
+            out,
             '<a class="sort-link" href="/search/?sort_by=-last_modified">Date Uploaded&nbsp;&darr;</a>')
 
     def test_apllied_filters_tag(self):
@@ -86,11 +89,12 @@ class TestTemplateTags(TestCase):
             'center_name': '(HMS-RK)',
             'library_strategy': '(AMPLICON OR CTS)',
             'last_modified': '[NOW-7DAY TO NOW]',
-            'disease_abbr': '(CNTL OR SARC)',
-            })
+            'disease_abbr': '(CNTL OR SARC)', })
         template = Template("{% load search_tags %}{% applied_filters request %}")
         result = template.render(RequestContext(request, {}))
-        self.assertEqual(result, 'Applied filter(s): <ul><li>Center: Harvard</li><li>Uploaded this week\
+        self.assertEqual(
+            result,
+            'Applied filter(s): <ul><li>Center: Harvard</li><li>Uploaded this week\
 </li><li>Disease: Controls, Sarcoma</li><li>Run Type: AMPLICON, CTS</li></ul>')
 
     def test_items_per_page_tag(self):
@@ -100,7 +104,8 @@ class TestTemplateTags(TestCase):
 
         request.GET = QueryDict('', mutable=False)
         template = Template(
-            "{% load pagination_tags %}{% items_per_page request " + str(default_limit) + " 100 %}")
+            "{% load pagination_tags %}{% items_per_page request " +
+            str(default_limit) + " 100 %}")
         result = template.render(RequestContext(request, {}))
         self.assertTrue('<a href="?limit=100">100</a>' in result)
         self.assertTrue(not default_limit_link in result)
@@ -110,12 +115,14 @@ class TestTemplateTags(TestCase):
         self.assertTrue(not '<a href="?limit=100">100</a>' in result)
         self.assertTrue(default_limit_link in result)
 
-        template = Template("{% load pagination_tags %}{% items_per_page request 10 'incorrect_data' %}")
+        template = Template(
+            "{% load pagination_tags %}{% items_per_page request 10 'incorrect_data' %}")
         try:
             result = template.render(RequestContext(request, {}))
             assert 'No exception raised for incorrect data'
         except Exception as e:
-            self.assertEqual(e.message,
+            self.assertEqual(
+                e.message,
                 "Limits can be numbers or it's string representation")
 
     def test_get_name_by_code_tag(self):
@@ -125,7 +132,7 @@ class TestTemplateTags(TestCase):
             for code, name in section_data['filters'].iteritems():
                 assert (get_name_by_code(section, code) == name)
 
-        assert (get_name_by_code('unknown_section', 'unknown_code') == 
+        assert (get_name_by_code('unknown_section', 'unknown_code') ==
                 'unknown_code')
 
 
@@ -153,7 +160,8 @@ class SearchViewPaginationTestCase(TestCase):
                 os.path.join(TEST_DATA_DIR, f),
                 os.path.join(CACHE_DIR, f)
             )
-        self.default_results = objectify.fromstring(open(os.path.join(CACHE_DIR, self.cache_files[0])).read())
+        self.default_results = objectify.fromstring(
+            open(os.path.join(CACHE_DIR, self.cache_files[0])).read())
         self.default_results_count = len(self.default_results.findall('Result'))
 
     def tearDown(self):
@@ -170,9 +178,10 @@ class SearchViewPaginationTestCase(TestCase):
         self.assertContains(response, 'Next')
 
     def test_pagination_one_page_limit_pagination(self):
-        response = self.client.get(reverse('search_page') +
-                                   '?q={query}&offset={offset}&limit={limit}'.format(
-                                       query=self.query, offset=0, limit=self.default_results_count))
+        response = self.client.get(
+            reverse('search_page') +
+            '?q={query}&offset={offset}&limit={limit}'.format(
+                query=self.query, offset=0, limit=self.default_results_count))
         self.assertContains(response, '1')
         self.assertContains(response, 'active')
         # Prev and Next are both disabled
@@ -193,8 +202,8 @@ class SearchViewPaginationTestCase(TestCase):
         Test redirect from home page if any GET parameters are specified.
         """
         response = self.client.get(
-                reverse('home_page')+'?q={query}'.format(query=self.query),
-                follow=True)
+            reverse('home_page') + '?q={query}'.format(query=self.query),
+            follow=True)
         self.assertTrue('search' in response.redirect_chain[0][0])
 
     def test_last_modified_if_no_q(self):
@@ -203,8 +212,8 @@ class SearchViewPaginationTestCase(TestCase):
         Search with last 7 days.
         """
         response = self.client.get(
-                reverse('search_page')+
-                    '?center_name={center_name}'.format(center_name='%28BCM%29'),
-                follow=True)
+            reverse('search_page') +
+            '?center_name={center_name}'.format(center_name='%28BCM%29'),
+            follow=True)
         self.assertTrue('last_modified' in response.redirect_chain[0][0])
         self.assertTrue('7DAY' in response.redirect_chain[0][0])
