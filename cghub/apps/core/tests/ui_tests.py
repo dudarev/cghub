ae047d2231c1e6d6060e80463c99b4cdc282f4d8
Artem Dudarev <dudarev@gmail.com>
2012-09-28 15:57:40 +0300

    ticket:89 two more methods to test sidebar (selecting dates, correct url)

--- cghub/apps/core/tests/ui_tests.py
+++ cghub/apps/core/tests/ui_tests.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 import time
 import os
+import re
 import shutil
 from wsapi.settings import CACHE_DIR
 from lxml import objectify
@@ -15,7 +16,7 @@ class SidebarTests(LiveServerTestCase):
         self.selenium.implicitly_wait(5)
         super(SidebarTests, self).setUpClass()
 
-    def test_(self):
+    def test_select_all(self):
         driver = self.selenium
         driver.get(self.live_server_url)
         driver.find_element_by_css_selector("span.ui-dropdownchecklist-text").click()
@@ -23,16 +24,122 @@ class SidebarTests(LiveServerTestCase):
         # by center has 7 centers, i0 - deselect all, i1-i7 - selections
         driver.find_element_by_id("ddcl-6-i0").click()
         for i in range(1, 8):
-            print i
             cb = driver.find_element_by_id("ddcl-6-i%d" % i)
             self.assertFalse(cb.is_selected())
 
         # click again - select all
         driver.find_element_by_id("ddcl-6-i0").click()
         for i in range(1, 8):
-            print i
             cb = driver.find_element_by_id("ddcl-6-i%d" % i)
             self.assertTrue(cb.is_selected())
+
+    def test_select_date(self):
+        driver = self.selenium
+        driver.get(self.live_server_url)
+        driver.find_element_by_css_selector("span.ui-dropdownchecklist-text").click()
+
+        # dates are ddcl-4
+        driver.find_element_by_xpath(
+            "//span[@id='ddcl-4']/span/span").click()
+
+        # click the first selection
+        i_click = 0
+
+        # check that others are not selected
+        driver.find_element_by_id("ddcl-4-i%d" % i_click).click()
+        for i in range(0, 5):
+            if not i == i_click:
+                rb = driver.find_element_by_id("ddcl-4-i%i" % i)
+                self.assertFalse(rb.is_selected())
+
+        # click the second selection
+        i_click = 1
+
+        # check that others are not selected
+        driver.find_element_by_id("ddcl-4-i%d" % i_click).click()
+        for i in range(0, 5):
+            if not i == i_click:
+                rb = driver.find_element_by_id("ddcl-4-i%i" % i)
+                self.assertFalse(rb.is_selected())
+
+    def test_selection(self):
+        driver = self.selenium
+        driver.get(self.live_server_url)
+
+        driver.find_element_by_css_selector("span.ui-dropdownchecklist-text").click()
+        # center: BCM+OR+WUGSC+OR+BCCAGSC
+        driver.find_element_by_id("ddcl-6-i0").click()
+        driver.find_element_by_id("ddcl-6-i1").click()
+        driver.find_element_by_id("ddcl-6-i2").click()
+        driver.find_element_by_id("ddcl-6-i3").click()
+        driver.find_element_by_xpath("//div[@id='ddcl-6-ddw']/div/div[9]").click()
+        driver.find_element_by_xpath("//span[@id='ddcl-5']/span/span").click()
+        # sample type: 02+OR+03+OR+13
+        driver.find_element_by_id("ddcl-5-i0").click()
+        driver.find_element_by_id("ddcl-5-i1").click()
+        driver.find_element_by_id("ddcl-5-i2").click()
+        driver.find_element_by_id("ddcl-5-i3").click()
+        driver.find_element_by_xpath("//div[@id='ddcl-5-ddw']/div/div[21]").click()
+        driver.find_element_by_xpath("//span[@id='ddcl-3']/span/span").click()
+        # disease_abbr: ESCA+OR+DLBC+OR+READ
+        driver.find_element_by_id("ddcl-3-i0").click()
+        driver.find_element_by_id("ddcl-3-i1").click()
+        driver.find_element_by_id("ddcl-3-i2").click()
+        driver.find_element_by_id("ddcl-3-i3").click()
+        driver.find_element_by_xpath("//div[@id='ddcl-3-ddw']/div/div[29]").click()
+        driver.find_element_by_xpath("//span[@id='ddcl-2']/span/span").click()
+        # analyte_code: D+OR+G+OR+H
+        driver.find_element_by_id("ddcl-2-i0").click()
+        driver.find_element_by_id("ddcl-2-i1").click()
+        driver.find_element_by_id("ddcl-2-i2").click()
+        driver.find_element_by_id("ddcl-2-i3").click()
+        driver.find_element_by_xpath("//div[@id='ddcl-2-ddw']/div/div[9]/span").click()
+        driver.find_element_by_xpath("//span[@id='ddcl-1']/span/span").click()
+        # library_strategy: POOLCLONE+OR+WCS+OR+EST
+        driver.find_element_by_id("ddcl-1-i0").click()
+        driver.find_element_by_id("ddcl-1-i1").click()
+        driver.find_element_by_id("ddcl-1-i2").click()
+        driver.find_element_by_id("ddcl-1-i3").click()
+        driver.find_element_by_xpath("//div[@id='ddcl-1-ddw']/div/div[22]/span").click()
+        driver.find_element_by_id("id_apply_filters").click()
+
+        url = driver.current_url
+
+        # center: BCM+OR+WUGSC+OR+BCCAGSC
+        self.assertTrue(
+            'BCM' in url and
+            'WUGSC' in url and
+            'BCCAGSC' in url)
+        self.assertFalse(
+            'UNC-LCCC' in url)
+        # sample type: 02+OR+03+OR+13
+        self.assertTrue(
+            re.match('.*sample_type=[^&]*02', url) and
+            re.match('.*sample_type=[^&]*03', url) and
+            re.match('.*sample_type=[^&]*13', url))
+        self.assertFalse(
+            re.match('.*sample_type=[^&]*14', url))
+        # disease_abbr: ESCA+OR+DLBC+OR+READ
+        self.assertTrue(
+            re.match('.*disease_abbr=[^&]*ESCA', url) and
+            re.match('.*disease_abbr=[^&]*DLBC', url) and
+            re.match('.*disease_abbr=[^&]*READ', url))
+        self.assertFalse(
+            re.match('.*disease_abbr=[^&]*GBM', url))
+        # analyte_code: D+OR+G+OR+H
+        self.assertTrue(
+            re.match('.*analyte_code=[^&]*D', url) and
+            re.match('.*analyte_code=[^&]*G', url) and
+            re.match('.*analyte_code=[^&]*H', url))
+        self.assertFalse(
+            re.match('.*analyte_code=[^&]*T', url))
+        # library_strategy: POOLCLONE+OR+WCS+OR+EST
+        self.assertTrue(
+            re.match('.*library_strategy=[^&]*POOLCLONE', url) and
+            re.match('.*library_strategy=[^&]*WCS', url) and
+            re.match('.*library_strategy=[^&]*EST', url))
+        self.assertFalse(
+            re.match('.*library_strategy=[^&]*WXS', url))
 
     @classmethod
     def tearDownClass(self):
