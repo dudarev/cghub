c1d72991b694ed1e0d2754898237df1b627dac14
Oleksandr Polyeno <polyenoom@gmail.com>
2012-10-23 12:34:20 +0300

    ticket:120 Added test for help views

--- cghub/apps/help/tests.py
+++ cghub/apps/help/tests.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from cghub.apps.help.urls import urlpatterns
+
 
 class HelpTest(TestCase):
+
+    def test_help_views(self):
+        for pattern in urlpatterns:
+            if (
+                    pattern.name and
+                    pattern.name.startswith('help') and
+                    pattern.name.endswith('page')):
+                response = self.client.get(reverse(pattern.name))
+                self.assertEqual(response.status_code, 200)
 
     def test_links_to_home(self):
         response = self.client.get(reverse('help_cart_page'))
