c37276c92d520170b57d9ba29ad3bf63f4e09e62
Oleksandr Polyeno <polyenoom@gmail.com>
2012-10-23 17:02:30 +0300

    ticket:121 Fixed bug in activeItemDetailsLink function, added highlight.js, show raw xml after details table

--- cghub/static/js/base.js
+++ cghub/static/js/base.js
@@ -11,6 +11,7 @@ jQuery(function ($) {
         init:function () {
             cghub.base.cacheElements();
             cghub.base.bindEvents();
+            cghub.base.highlightCode();
         },
         cacheElements:function () {
             cghub.base.$navbarAnchors = $('div.navbar ul.nav li a');
@@ -53,17 +54,26 @@ jQuery(function ($) {
         },
         activateItemDetailsLinks:function () {
             $('.js-item-details-link').live('click', function(obj){
-                var link=$(obj.target);
-                var modal=$(link.attr('data-target'));
+                var link = $(obj.target);
+                var modal = $(link.attr('data-target'));
+                var loaded = false;
                 modal.on('shown', function(){
-                    modal.find('.modal-body').load(link.attr('href'));
+                    if (!loaded){
+                        modal.find('.modal-body').load(link.attr('href'), function(){
+                            $('.modal-body pre.xml-code').each(function(i, e) {hljs.highlightBlock(e)});
+                            loaded = true;
+                        });
+                    };
                 }).on('show', function(){
                     modal.find('.modal-body').html('Loading ...');
                     modal.find('.modal-label').html('Details for UUID='+link.text());
                 }).modal('show');
                 return false;
             });
-        }
+        },
+        highlightCode:function() {
+            $('pre.xml-code').each(function(i, e) {hljs.highlightBlock(e)});
+        },
     };
     cghub.base.init();
 });
