25121f64ace0d145d6f376d974a3fc855dc4dd91
Oleksandr Polyeno <polyenoom@gmail.com>
2012-10-23 12:16:21 +0300

    ticket:120 Added new help content

--- cghub/apps/help/urls.py
+++ cghub/apps/help/urls.py
@@ -5,8 +5,19 @@ from cghub.apps.help.views import HelpView
 
 urlpatterns = patterns('',
     url(r'^$', HelpView.as_view(), name='help_page'),
-    url(r'^search/$', HelpView.as_view(), name='help_search_page',
-                                kwargs={'template': 'help/search.html'}),
+    url(r'^overview/$', HelpView.as_view(), name='help_overview_page',
+                                kwargs={'template': 'help/overview.html'}),
+    url(r'^navbar/$', HelpView.as_view(), name='help_navbar_page',
+                                kwargs={'template': 'help/navbar.html'}),
+    url(r'^results/$', HelpView.as_view(), name='help_results_page',
+                                kwargs={'template': 'help/results.html'}),
+    url(r'^usecase/$', HelpView.as_view(), name='help_usecase_page',
+                                kwargs={'template': 'help/usecase.html'}),
+    url(r'^filters/$', HelpView.as_view(), name='help_filters_page',
+                                kwargs={'template': 'help/filters.html'}),
     url(r'^cart/$', HelpView.as_view(), name='help_cart_page',
                                 kwargs={'template': 'help/cart.html'}),
+    # TODO: is it used?
+    url(r'^search/$', HelpView.as_view(), name='help_search_page',
+                                kwargs={'template': 'help/search.html'}),
 )
