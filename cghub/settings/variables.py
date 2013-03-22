6e2bd2a008b936c4e04c8869db3ba4e1d4fc5453
Oleksandr Polyeno <polyenoom@gmail.com>
2013-03-22 17:17:05 +0200

    ticket:203 specify COOKIE_MAX_AGE manualy instead of using total_seconds

--- cghub/settings/variables.py
+++ cghub/settings/variables.py
@@ -1,7 +1,7 @@
-import datetime
-
 # also specified in search.js
 LAST_QUERY_COOKIE = 'last_query'
 
 # max_age should be a number of seconds, or None (default) if the cookie should last only as long as the client's browser session
-COOKIE_MAX_AGE = datetime.timedelta(days=365).total_seconds()
+# datetime.timedelta(days=365).total_seconds()
+# 31536000.0
+COOKIE_MAX_AGE = 31536000
