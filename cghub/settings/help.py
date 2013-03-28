0a8425b741753819e0c237beb18498edfc35e58b
Marina Solovyova <marinka.solovyova@gmail.com>
2013-03-29 00:17:39 +0200

    ticket:214 tooltip for one selected filter value

--- cghub/settings/help.py
+++ cghub/settings/help.py
@@ -8,4 +8,8 @@ keys have to be like this:
 HELP_HINTS = {
     'UUID': 'Help for UUID, and this is <a href="#" class="js-help-link" data-slug="uuid-help">link</a>, click to view help page!',
     'Study:TCGA': 'Some help for "Study/TCGA"',
+    'Study:TCGA Benchmark': 'Some help for "Study/TCGA Benchmark"',
+
+    'Center:Baylor': 'Help tooltip for "Center: Baylor"',
+    'Center:BCCAGSC': 'Help tooltip for "Center: BCCAGSC"',
 }
