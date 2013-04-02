76e47e224d7482291cc4ac101c8b01412ac7d094
Oleksandr Polyeno <polyenoom@gmail.com>
2013-04-02 17:17:51 +0300

    ticket:219 fixed query calculation in selectfilters.py

--- cghub/apps/core/management/commands/selectfilters.py
+++ cghub/apps/core/management/commands/selectfilters.py
@@ -107,7 +107,7 @@ class Command(BaseCommand):
                     self.stdout.write('checking filter\n')
                     is_filter_used[(key, filter)] = False
                     for date in DATE_RANGES:
-                        query = '%s=%s&%s' % (key, filter, date)
+                        query = '%s=(%s)&%s' % (key, filter, date)
                         self.stdout.write('query: %s\n' % query)
                         results = api_request(query=query, get_attributes=False)
                         hits = int(results.Hits.text)
