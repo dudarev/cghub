079219efedef998866da3ea957c5be4da2e491e6
Oleksandr Polyeno <polyenoom@gmail.com>
2013-04-10 21:59:32 +0300

    ticket:230 removed sample tooltips

--- cghub/settings/help.py
+++ cghub/settings/help.py
@@ -11,10 +11,4 @@ HELP_HINTS = {
     'filter:Study': 'Sample tooltip for filter "Study"',
     'Study:TCGA': 'Sample tooltip for "Study/TCGA"',
     'Study:TCGA Benchmark': 'Sample tooltip for "Study/TCGA Benchmark"',
-
-    'common:cghub-home-page-link': 'Sample tooltip for cghub-home-page link',
-    'common:search-link': 'Sample tooltip for search link',
-    'common:cart-link': 'Sample tooltip for cart link',
-    'common:help-link': 'Sample tooltip for help link',
-    'common:search-box': 'Sample tooltip for search box',
 }
