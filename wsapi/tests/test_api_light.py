b51a2251c5b171fb7403e2bd9975235ba2b50881
Oleksandr Polyeno <polyenoom@gmail.com>
2013-04-15 18:25:14 +0300

    ticket:236 fixed test_get_all_id

--- wsapi/tests/test_api_light.py
+++ wsapi/tests/test_api_light.py
@@ -27,7 +27,7 @@ class ApiLightTest(unittest.TestCase):
         filepath = get_cache_file_name(query='xml_text=6d51*', settings={})
         self.assertEqual(
                 filepath,
-                '%s47fc9c0916a570ed7970e98508a07a60.ids' % get_setting('CACHE_DIR'))
+                '%sec0d99a4758829215ef1d7730bb19eb4.ids' % get_setting('CACHE_DIR'))
 
     def test_get_ids(self):
         """
@@ -37,7 +37,7 @@ class ApiLightTest(unittest.TestCase):
         cache_dir = get_setting('CACHE_DIR')
         if not os.path.exists(cache_dir):
             os.makedirs(cache_dir)
-        f = '47fc9c0916a570ed7970e98508a07a60.ids'
+        f = 'ec0d99a4758829215ef1d7730bb19eb4.ids'
         shutil.copy(
                 os.path.join(TEST_DATA_DIR, f),
                 os.path.join(cache_dir, f))
@@ -55,7 +55,7 @@ class ApiLightTest(unittest.TestCase):
         cache_dir = get_setting('CACHE_DIR')
         if not os.path.exists(cache_dir):
             os.makedirs(cache_dir)
-        f = '47fc9c0916a570ed7970e98508a07a60.ids'
+        f = 'ec0d99a4758829215ef1d7730bb19eb4.ids'
         shutil.copy(
                 os.path.join(TEST_DATA_DIR, f),
                 os.path.join(cache_dir, f))
@@ -68,7 +68,7 @@ class ApiLightTest(unittest.TestCase):
         # 47fc9c0916a570ed7970e98508a07a60.ids - 'xml_text=6d51*' unsorted
         # only 47fc9c0916a570ed7970e98508a07a60.ids contains
         # '9b62b3c9-e33a-4736-8e65-777777777777'
-        f = '3c021199146d698d0a72ddbbfb927c41.ids'
+        f = '1e0d4e80b3c68459fa38ac23185faae7.ids'
         shutil.copy(
                 os.path.join(TEST_DATA_DIR, f),
                 os.path.join(cache_dir, f))
