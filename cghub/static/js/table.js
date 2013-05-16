5305cde27f262bcc2f08f51e10cf4846e31b21ec
Oleksandr Polyeno <polyenoom@gmail.com>
2013-05-16 22:57:17 +0300

    ticket:287 fixed table scrolling when selected cell invisible

--- cghub/static/js/table.js
+++ cghub/static/js/table.js
@@ -26,9 +26,9 @@ jQuery(function ($) {
             cghub.table.$itemsPerPageLink.on('click', cghub.table.saveSelectedFiles);
             cghub.table.$selectAllCheckbox.on('change', cghub.table.changeCheckboxes);
             cghub.table.$checkboxes.on('change', cghub.table.updateSelectAll);
-            cghub.table.$checkboxes.on('focusin', cghub.table.focusInOutCheckbox);
-            cghub.table.$checkboxes.on('focusout', cghub.table.focusInOutCheckbox);
-            cghub.table.$flexigrid.on('keydown', cghub.table.arrowAltPress);
+            cghub.table.$checkboxes.on('focusin', cghub.table.tableCheckboxFocus);
+            cghub.table.$checkboxes.on('focusout', cghub.table.tableCheckboxFocus);
+            cghub.table.$flexigrid.on('keydown', cghub.table.arrowPress);
         },
         activateItemDetailsLinks:function () {
             $(document).on('click', '.bDiv tr', function(obj){
@@ -99,20 +99,20 @@ jQuery(function ($) {
             )
             $.cookie('browser_checked_items', selected_items.join(','), { path: '/', expires: 7 });
         },
-        focusInOutCheckbox:function(event){
-            if (event.type == "focusin"){
+        tableCheckboxFocus:function(event){
+            if (event.type == "focusin" && !$('.tdSelected').length){
                 $(this).parents('td').addClass("tdSelected");
             }
-            else if (event.type == "focusout"){
-                $(this).parents('td').removeClass("tdSelected");
-                $('td.tdSelected').removeClass("tdSelected");
+            else if (event.type == "focusout" && $(this).parents('td').hasClass("tdSelected")) {
+                $('.tdSelected').removeClass("tdSelected");
             }
         },
-        arrowAltPress:function(event){
-            //arrowLeft: 37
-            //arrowUp: 38
-            //arrowRight: 39
-            //arrowDown: 40
+        arrowPress:function(event){
+            /* alt + arrow press handler
+            arrowLeft: 37
+            arrowUp: 38
+            arrowRight: 39
+            arrowDown: 40 */
             if(!event.altKey) return;
             var charCode = (event.which) ? event.which : event.keyCode;
             if($.inArray(charCode, [37, 39, 38, 40]) == -1) return;
@@ -147,32 +147,24 @@ jQuery(function ($) {
             }
 
             if(!nextStep.is('td')) return false;
-            currentCell.removeClass('tdSelected');
+            $('.tdSelected').removeClass("tdSelected");
             nextStep.addClass('tdSelected');
 
+            cghub.table.scrollTableToCell(nextStep);
+            
             return false;
-
-            cghub.table.makeVisibleOnScreen(nextStep);
         },
-
-        makeVisibleOnScreen: function(target){
-            // check if an element is visible on screen, if not - adjust the scrollbars
-            // scroll to right
-            while ($(target).offset().left + $(target).width() >
-                   cghub.table.$flexigrid.offset().left + cghub.table.$flexigrid.width()){
-                cghub.table.$tableContainer.scrollLeft(cghub.table.$tableContainer.scrollLeft()+100);
+        scrollTableToCell:function(target){
+            /* is cell fully visible ? */
+            var cell_left = $(target).offset().left;
+            var cell_right = cell_left + $(target).width();
+            var table_left = cghub.table.$flexigrid.offset().left;
+            var table_right = table_left + cghub.table.$flexigrid.width();
+            if(cell_right > table_right) {
+                cghub.table.$tableContainer.scrollLeft(cghub.table.$tableContainer.scrollLeft() + cell_right - table_right + 50);
             }
-            // scroll to left
-            while ($(target).offset().left < cghub.table.$flexigrid.offset().left) {
-                cghub.table.$tableContainer.scrollLeft(cghub.table.$tableContainer.scrollLeft()-100);
-            }
-            // scroll down
-            while ($(target).offset().top + $(target).height() - $(window).scrollTop() > $(window).height()){
-                $(window).scrollTop($(window).scrollTop() + 200);
-            }
-            // scroll up
-            while ($(target).offset().top - $(window).scrollTop() < $('.navbar').height()){
-                $(window).scrollTop($(window).scrollTop() - 200);
+            if(cell_left < table_left) {
+                cghub.table.$tableContainer.scrollLeft(cghub.table.$tableContainer.scrollLeft() - table_left + cell_left - 50);
             }
         }
     }
