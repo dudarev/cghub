191dd7c2b2b04f239e49f9ddf6fbfd802b58893a
Oleksandr Polyeno <polyenoom@gmail.com>
2013-05-17 14:44:22 +0300

    ticket:292 fixed flexigrid code that adjusts last column width

--- cghub/static/js/libs/flexigrid.js
+++ cghub/static/js/libs/flexigrid.js
@@ -275,7 +275,7 @@
             },
             adjustTableWidth: function () {
                 var $hDivBox = $('.hDivBox');
-                var widthDiff = $hDivBox.parents('.viewport').width() - 2 - $hDivBox.width();
+                var widthDiff = $hDivBox.parent().width() - $hDivBox.width();
                 var $oldLast = $('th div.flLastCol', this.hDiv);
                 var $newLast = $('th:visible:last div', this.hDiv);
                 if($oldLast && $newLast) {
@@ -294,7 +294,7 @@
                         this.rePosDrag();
                         this.fixHeight();
                         $oldLast.removeClass('flLastCol');
-                        widthDiff = $hDivBox.parents('.viewport').width() - 2 - $hDivBox.width();
+                        widthDiff = $hDivBox.parent().width() - $hDivBox.width();
                     }
                 }
                 var addWidth = 0;
@@ -320,8 +320,8 @@
                 this.hDiv.scrollLeft = this.bDiv.scrollLeft;
                 this.rePosDrag();
                 this.fixHeight();
-                widthDiff = $hDivBox.parents('.viewport').width() - 2 - $hDivBox.width();
-                $('.bDiv').children().width($hDivBox.width());
+                widthDiff = $hDivBox.parent().width() - $hDivBox.width();
+                $('.bDiv').children().width($hDivBox.width() - 1);
             },
             toggleCol: function (cid, visible) {
                 var ncol = $("th[axis='col" + cid + "']", this.hDiv)[0];
