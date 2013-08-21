83bb412e10f14afd86929713f8cb6fa286548547
Oleksandr Polyeno <polyenoom@gmail.com>
2013-08-21 14:13:18 +0300

    ticket:358 remove xml header from analysis_xml if it present in solr result

---
+++ cghub/apps/core/templatetags/core_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def without_header(value):
+    """
+    Remove xml header like this:
+    <?xml version="1.0" encoding="ASCII" standalone="yes"?>
+    """
+    if value.find('<?') == 0:
+        start = value.find('?>')
+        if start != -1:
+            return value[start + 2:]
+    return value
