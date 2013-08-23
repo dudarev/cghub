7d54a6ca1511f410933465cc56f02959db59a9df
Oleksandr Polyeno <polyenoom@gmail.com>
2013-08-23 10:56:50 +0300

    ticket:367 fixed without_header templatetag

--- cghub/apps/core/templatetags/core_tags.py
+++ cghub/apps/core/templatetags/core_tags.py
@@ -10,6 +10,8 @@ def without_header(value):
     Remove xml header like this:
     <?xml version="1.0" encoding="ASCII" standalone="yes"?>
     """
+    if value is None:
+        return u''
     if value.find('<?') == 0:
         start = value.find('?>')
         if start != -1:
